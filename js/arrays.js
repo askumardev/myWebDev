98845241ff208f7c018e37f110cc70d0cc3f44e3
askumardev <askumar.dev@outlook.com>
2023-12-25 14:20:11 +0530

    array methods examples

---
+++ js/arrays.js
@@ -0,0 +1,28 @@
+const items = [
+  { name: 'Bike', price: 100 },
+  { name: 'TV', price: 200 },
+  { name: 'Album', price: 10 },
+  { name: 'Book', price: 5 },
+  { name: 'Phone', price: 500 },
+  { name: 'Computer', price: 1000 },
+  { name: 'KeyBoard', price: 25 }
+]
+
+const filteredItems = items.filter((item) => {
+  return item.price <= 100
+})
+console.log(filteredItems)
+
+const itemNames = items.map((item) => {
+  return item.name
+})
+console.log(itemNames)
+
+const foundNames = items.find((item) => {
+  return item.name === 'Book'
+})
+console.log(foundNames)
+
+items.forEach((item) => {
+  console.log(item.name)
+})
