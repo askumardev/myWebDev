2d2e410dc080d3e96aa96e169f6d03c2d7d7787c
askumardev <askumar.dev@outlook.com>
2024-07-01 13:23:44 +0530

    sort example

---
+++ js/sort.js
@@ -0,0 +1,25 @@
+const items = [
+  { name: "Edward", value: 21 },
+  { name: "Sharpe", value: 37 },
+  { name: "And", value: 45 },
+  { name: "The", value: -12 },
+  { name: "Magnetic", value: 13 },
+  { name: "Zeros", value: 37 },
+];
+  
+// Sort by name first, then by value
+items.sort((a, b) => {
+  const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  // If names are the same, compare by value
+  return a.value - b.value;
+});
+
+console.log(items);
