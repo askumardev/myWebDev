a30b857f70aed3c1cc8ae00ccb0d74868cad60c3
askumardev <askumar.dev@outlook.com>
2024-07-10 16:27:55 +0530

    var errors in js

--- js/let_const_var.js
+++ js/let_const_var.js
@@ -1,17 +1,28 @@
 //node js/let_const_var.js
 
-console.log(a);
+
+// console.log(y); //ReferenceError: y is not defined  
+console.log(a); //undefined
 //console.log(b); //ReferenceError: Cannot access 'b' before initialization
 //console.log(c); ReferenceError: Cannot access 'c' before initialization 
 
 var a = 10;
+// var a = 15; //No error
 let b = 20;
+//let b = 40; SyntaxError: Identifier 'b' has already been declared
 const c = 30;
+// const c = 50; //SyntaxError: Identifier 'c' has already been declared 
+// const d; SyntaxError: Missing initializer in const declaration
+// d=100;
+// console.log(d);
 
 console.log(a);
 console.log(b); 
 console.log(c);
 
-// let a = 100; //SyntaxError: Identifier 'a' has already been declared 
-// console.log(a);
+b = 100; 
+console.log(b);
+
+// c = 200; //TypeError: Assignment to constant variable.
+// console.log(c);
 
