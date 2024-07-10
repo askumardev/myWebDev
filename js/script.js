c70e5880d7cbc318fbbf865418d8cee96ff07276
askumardev <askumar.dev@outlook.com>
2024-07-10 15:36:09 +0530

    script js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 // node js/script.js
 
 messaging('Hello...')
-
 messaging(5)
-
 messaging('something')
-
 message = 'abcd'
-
 messaging(message)
 
 function messaging(message){
   console.log(message)
+}
+
+var a;
+console.log(a);
+if(a === undefined){
+  console.log("a is undefined")
+} else {
+  console.log("a is not undefined")
 }
