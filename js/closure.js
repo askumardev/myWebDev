f9c3d46302f8a9d4239d683ae020a29393b4867a
askumardev <askumar.dev@outlook.com>
2024-07-10 17:20:23 +0530

    closure js

---
+++ js/closure.js
@@ -0,0 +1,20 @@
+// node js/closure.js
+
+function a(){
+  var a = 10;
+  function y(){
+    console.log(a);
+  }
+  y();
+}
+a();
+
+function x(){
+  var a = 10;
+  function y(){
+    console.log(a);
+  }
+  return y
+}
+var z = x();
+console.log(z);
