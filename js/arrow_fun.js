930595d4ec42a2c685a13f95f6dc0834ce19882a
askumardev <askumar.dev@outlook.com>
2024-07-10 16:12:29 +0530

    let const var js

--- js/arrow_fun.js
+++ js/arrow_fun.js
@@ -4,3 +4,5 @@ const materials = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium'];
 
 console.log(materials.map((material) => material.length));
 // Expected output: Array [8, 6, 7, 9]
+
+console.log(materials.sort());
---
+++ js/let_const_var.js
@@ -0,0 +1,13 @@
+//node js/let_const_var.js
+
+console.log(a);
+//console.log(b); //ReferenceError: Cannot access 'b' before initialization
+//console.log(c); ReferenceError: Cannot access 'c' before initialization 
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+console.log(a);
+console.log(b); 
+console.log(c);
