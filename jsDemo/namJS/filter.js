8af95ad2f30d9fecc31b223bbe8373cdb666d7cf
askumardev <askumar.dev@outlook.com>
2024-08-05 11:43:38 +0530

    array filter

--- jsDemo/namJS/filter.js
+++ jsDemo/namJS/filter.js
@@ -1,3 +1,31 @@
 // node jsDemo/namJS/filter.js
 
 const arr = [5,1,3,2,6];
+
+console.log("------isOdd filter----");
+function isOdd(x){
+  return x % 2;
+}
+
+let output = arr.filter(isOdd);
+console.log(output);
+
+console.log("------isEven filter----");
+function isEven(x){
+  return x % 2 === 0;
+}
+
+output = arr.filter(isEven);
+console.log(output);
+
+console.log("------greater4 filter----");
+output = arr.filter(function greater4(x){
+  return x > 4;
+});
+console.log(output);
+
+console.log("------arrow function to show less than 4 filter----");
+output = arr.filter((x) => {
+  return x < 4;
+});
+console.log(output);
