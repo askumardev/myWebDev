ae280f511683a3ed6c4d201bdb76da67f5f35b8f
askumardev <askumar.dev@outlook.com>
2024-08-05 14:43:46 +0530

    promise

--- jsDemo/namJS/promise.js
+++ jsDemo/namJS/promise.js
@@ -1,11 +1,25 @@
 // node jsDemo/namJS/promise.js
 
 
-let promise = new Promise((resolve, reject) => {
-  console.log("calling promise..");
- // resolve("success");
-  reject("something wrong happened");
+// let promise = new Promise((resolve, reject) => {
+//   console.log("calling promise..");
+//  // resolve("success");
+//   reject("something wrong happened");
 
-})
+// })
 
-console.log(promise);
+// console.log(promise);
+
+
+function getData(id, getNextData){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data:", id);
+      resolve("success");
+      if (getNextData){
+        getNextData();
+      } 
+    }, 5000);
+  });  
+}
+//let final = getData(123); in console and check final
