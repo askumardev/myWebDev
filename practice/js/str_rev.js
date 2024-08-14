f3b115603600c50a62b7916654cfdae53c236b32
askumardev <askumar.dev@outlook.com>
2024-08-14 13:37:23 +0530

    Signed-off-by: askumardev <askumar.dev@outlook.com>

---
+++ practice/js/str_rev.js
@@ -0,0 +1,13 @@
+let str = 'satish'
+var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+
+let rev = ""
+
+for (var i = str.length - 1; i >= 0; i--) {
+        rev += str[i];
+    }
+console.log(rev);
+
+
+// node practice/js/str_rev.js  
